cfd51f4f024779cf9dd1b7e73c7341b81f8baccf
Iftifar Taz <iftifar.taz@northsouth.edu>
2025-01-14 18:04:46 +0600

    routing restructure

---
+++ EmpTracker.ApiGateway/src/Configurations/RateLimitConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.RateLimiting;
+
+namespace EmpTracker.ApiGateway.Configurations
+{
+    public static class ApplicationConfiguration
+    {
+        public static IServiceCollection ConfigureRateLImiting(this IServiceCollection services)
+        {
+            services.AddRateLimiter(options =>
+            {
+                options.AddSlidingWindowLimiter("slidingWindowLimiterPolicy", opt =>
+                {
+                    opt.PermitLimit = 4;
+                    opt.Window = TimeSpan.FromSeconds(30);
+                    opt.SegmentsPerWindow = 1;
+                    opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                    opt.QueueLimit = 1;
+                });
+                options.OnRejected = (context, _) =>
+                {
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                    context.HttpContext.Response.ContentType = "application/json";
+                    context.HttpContext.Response.WriteAsJsonAsync(new { message = "Too many requests. Please try again later." });
+                    return new ValueTask();
+                };
+            });
+            return services;
+        }
+    }
+}
--- EmpTracker.ApiGateway/src/Program.cs
+++ EmpTracker.ApiGateway/src/Program.cs
@@ -1,55 +1,11 @@
-using System.Text;
-using System.Threading.RateLimiting;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.RateLimiting;
-using Microsoft.IdentityModel.Tokens;
+using EmpTracker.ApiGateway.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-builder.Services.AddRateLimiter(options =>
-{
-    options.AddSlidingWindowLimiter("slidingWindowLimiterPolicy", opt =>
-    {
-        opt.PermitLimit = 4;
-        opt.Window = TimeSpan.FromSeconds(30);
-        opt.SegmentsPerWindow = 1;
-        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-        opt.QueueLimit = 1;
-    });
-    options.OnRejected = (context, _) =>
-    {
-        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
-        context.HttpContext.Response.ContentType = "application/json";
-        context.HttpContext.Response.WriteAsJsonAsync(new { message = "Too many requests. Please try again later." });
-        return new ValueTask();
-    };
-});
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-var jwtSettings = builder.Configuration.GetSection("JWTSetting");
-
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.SaveToken = true;
-    x.RequireHttpsMetadata = false;
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ClockSkew = TimeSpan.Zero,
-        ValidAudience = jwtSettings.GetSection("ValidAudience").Value!,
-        ValidIssuer = jwtSettings.GetSection("ValidIssuer").Value!,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.GetSection("SecurityKey").Value!))
-    };
-});
+builder.Services.ConfigureRateLImiting();
+builder.Services.ConfigureJwtAuthentication(builder.Configuration);
 
 builder.Services.AddAuthorization();
 
@@ -57,8 +13,7 @@ var app = builder.Build();
 
 app.UseRateLimiter();
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseJwtAuthentication();
 
 app.MapReverseProxy();
 app.Run();
